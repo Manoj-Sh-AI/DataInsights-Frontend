5d09830c1bf40abd3e82593da20f8ac296acb24a
Manoj-Sh-AI <shmanoj100@gmail.com>
2024-04-26 13:54:20 +0530

    10th commit

--- src/components/CrimeSecond/CrimeSecond.jsx
+++ src/components/CrimeSecond/CrimeSecond.jsx
@@ -15,16 +15,19 @@ const CrimeSecond = () => {
       <div className="years">
         <div className="year">
           <h3>Crime Against <br />Women</h3>
+          <br />
           <a href="https://app.powerbi.com/reportEmbed?reportId=a29cdce9-e858-4d22-9031-41ef552869e6&autoAuth=true&ctid=5e6e043f-b14f-4f58-9b38-939a1393e404" target="_blank" rel="noopener noreferrer" class="new_btn" data-hidden-link>Open Dashboard</a>
           <p><br /></p>
         </div>
         <div className="year">
           <h3>Crime By Place of Occurrence</h3>
+          <br />
           <a href="https://app.powerbi.com/reportEmbed?reportId=9add3c82-8f7f-455e-a957-127f99cacd89&autoAuth=true&ctid=5e6e043f-b14f-4f58-9b38-939a1393e404" target="_blank" rel="noopener noreferrer" class="new_btn" data-hidden-link>Open Dashboard</a>
           <p><br /></p>
         </div>
         <div className="year">
           <h3>Indian Penal Code (IPC) Crimes</h3>
+          <br />
           <a href="https://app.powerbi.com/reportEmbed?reportId=c40e8054-8079-46a7-9316-f39f56c5c061&autoAuth=true&ctid=5e6e043f-b14f-4f58-9b38-939a1393e404" target="_blank" rel="noopener noreferrer" class="new_btn" data-hidden-link>Open Dashboard</a>
           <p><br /></p>
         </div>
--- src/components/CrimeThird/CrimeThird.jsx
+++ src/components/CrimeThird/CrimeThird.jsx
@@ -14,6 +14,7 @@ const CrimeThird = () => {
       <h2>World Crime Index <a className='HeatMap'>Heat Map</a></h2>
         <div className="year">
           <h3>World Crime Index</h3>
+          <br />
           <a href="https://app.powerbi.com/reportEmbed?reportId=f3990e94-ee3e-4be8-8e72-b5d439d41063&autoAuth=true&ctid=5e6e043f-b14f-4f58-9b38-939a1393e404" target="_blank" rel="noopener noreferrer" class="new_btn" data-hidden-link>Open Dashboard</a>
           <p><br /></p>
         </div>
